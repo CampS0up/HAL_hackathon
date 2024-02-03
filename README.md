f9080a7bb83694fcb9b2944af2a5b92d96abcf72
Lucas Lamar <llamareo@gmail.com>
2024-02-03 17:03:21 -0600

    changed readme and made the listener a function

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ run this stuff to get your modules working
 pip install pyaudio
 pip install SpeechRecognition
 pip install pyttsx3
+
+
+## usage example
+import listener
+
+spoken_text = listener.listen()
+
+print(spoken_text)
--- listener.py
+++ listener.py
@@ -5,32 +5,43 @@
 import speech_recognition as sr
 import pyttsx3
 
+def listen():
+    
+    MyText = None
 
-while(1):    
-     
+    r = sr.Recognizer()
+
     # Exception handling to handle
     # exceptions at the runtime
     try:
-         
+            
         # use the microphone as source for input.
         with sr.Microphone() as source2:
+            
                 
             # wait for a second to let the recognizer
             # adjust the energy threshold based on
             # the surrounding noise level 
-            sr.adjust_for_ambient_noise(source2, duration=0.2)
-             
+            try:
+                print("adjusting to ambient noise")
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+                print("done")
+            except Exception as e:
+                print(f"error: ambient noise wasn't set{e}")
+                
             #listens for the user's input 
-            audio2 = sr.listen(source2)
-             
+            audio2 = r.listen(source2)
+            print("listening to voice")
+                
             # Using google to recognize audio
-            MyText = sr.recognize_google(audio2)
+            MyText = r.recognize_google(audio2)
             MyText = MyText.lower()
- 
-            print("Did you say ",MyText)
-             
+
+                
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
-         
+            
     except sr.UnknownValueError:
         print("unknown error occurred")
+
+    return MyText
