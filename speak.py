8d5eddff92b1aa1470aa593cd01be79023ea80a6
cbrown <campbrown123@gmail.com>
2024-02-03 22:01:35 -0600

    Added text to speak call
    SPEAK

--- speak.py
+++ speak.py
@@ -1,69 +1,12 @@
 from gtts import gTTS
-import os
 import playsound
 
 
 def text_to_speech(text):
-    tts = gTTS(text="I wanna die", lang='en')
- 
+    tts = gTTS(text, lang='en')
+
     tts.save("speak.mp3")
     playsound.playsound("speak.mp3")
 
-
-
-
-
-
-
-
-
-
-
-"""import speech_recognition as sr
-import pyttsx3 
- 
-# Initialize the recognizer 
-r = sr.Recognizer() 
- 
-# Function to convert text to
-# speech
-def SpeakText(command):
-     
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command) 
-    engine.runAndWait()
-     
-     
-# Loop infinitely for user to
-# speak
- 
-while(1):    
-     
-    # Exception handling to handle
-    # exceptions at the runtime
-    try:
-         
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-             
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level 
-            r.adjust_for_ambient_noise(source2, duration=0.2)
-             
-            #listens for the user's input 
-            audio2 = r.listen(source2)
-             
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
- 
-            print("Did you say ",MyText)
-            SpeakText(MyText)
-             
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-         
-    except sr.UnknownValueError:
-        print("unknown error occurred")"""
+# Example usage:
+text_to_speech("I wanna die")
