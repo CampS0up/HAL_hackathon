c58f75a99ccdbaf28ebacb228fb4c5a8484ce9d7
KeannaTennyson@ <ktennyson98@gmail.com>
2024-02-03 22:38:26 -0500

    Adding speech functionality
    SPEAK

--- speak.py
+++ speak.py
@@ -1,19 +1,69 @@
-#HAL_hackathon(speaker.py_text to speech)
 from gtts import gTTS
-import io
+import os
+import playsound
+
 
 def text_to_speech(text):
-    tts = gTTS(text=text_en, lang='en')
-   #tts = gTTS(text=text_es, lang='es')
-    #tts = gTTS(text=text_fr, lang='fr')
-    #tts = gTTS(text=text, lang='tlh')
-
+    tts = gTTS(text="I wanna die", lang='en')
+ 
     tts.save("speak.mp3")
-    os.system("mpg321 output.mp3")
+    playsound.playsound("speak.mp3")
 
-text_en = "Hello, how are you?"
-#text_es = "Hola, este es un mensaje en español."
-#text_fr = "Bonjour, ceci est un message en français."
-text_to_speech(text_en)
-#text_to_speech(text_es)
-#text_to_speech(text_fr)
+
+
+
+
+
+
+
+
+
+
+"""import speech_recognition as sr
+import pyttsx3 
+ 
+# Initialize the recognizer 
+r = sr.Recognizer() 
+ 
+# Function to convert text to
+# speech
+def SpeakText(command):
+     
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+     
+     
+# Loop infinitely for user to
+# speak
+ 
+while(1):    
+     
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+         
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+             
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level 
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+             
+            #listens for the user's input 
+            audio2 = r.listen(source2)
+             
+            # Using google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+ 
+            print("Did you say ",MyText)
+            SpeakText(MyText)
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+         
+    except sr.UnknownValueError:
+        print("unknown error occurred")"""
