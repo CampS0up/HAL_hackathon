a610be1d78e629d9ea6adf1f6d2602630b6bffd2
Lucas Lamar <llamareo@gmail.com>
2024-02-03 21:10:30 -0600

    made some rlly cool stuff that works yippee

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import chat
 import listener
 
-voice_input = listener.listen()
-chat.response(voice_input)
+voice_input = "" #i know this looks silly here but it'll break if you delte this line
+
+while True:
+    voice_input = listener.listen()
+    if ("open the pod bay doors hal" in voice_input):
+        print("I'msorry Dave. I cant do that.")
+        break
+
+while True:
+    voice_input = listener.listen()
+    if voice_input != None:
+        response = chat.response(voice_input)
+        print("generating response")
+        print(response)
