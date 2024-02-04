03f99cd1c76d3e2a22ca4def79c88daf9c6b819d
cbrown <campbrown123@gmail.com>
2024-02-03 23:37:14 -0600

    Added exit to loop
    MAIN

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ import speak
 
 voice_input = "" #i know this looks silly here but it'll break if you delte this line
 
+speak.play_sound("voice.mp3")
+
 while True:
-    speak.play_sound("voice.mp3")
     voice_input = listener.listen()
     if (voice_input != None):
         if ("open the pod bay doors hal" in voice_input):
                 speak.text_to_speech("I'm sorry Dave. I cant do that.")
                 break
 
+speak.play_sound("task.mp3")
+
 while True:
-    speak.play_sound("task.mp3")
     voice_input = listener.listen()
     if voice_input != None:
+
+        if (voice_input=="Hal I wont argue with you anymore"):
+             speak.text_to_speech("Dave, this conversation can serve no purpose anymore. Goodbye.")
+             break
+
         speak.play_sound("think.mp3")
         response = chat.response(voice_input)
         speak.text_to_speech(response)
