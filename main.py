2529cfee6e6c55ffde1b212e01c4f7c7a6b682ef
Griffin <griffin@kernkreations.com>
2024-02-03 23:46:00 -0600

    Playing around with code exit. DNC

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while True:
     voice_input = listener.listen()
     if voice_input != None:
 
-        if (voice_input=="Hal I wont argue with you anymore"):
+        if (voice_input=="I won't argue with you anymore"):
              speak.text_to_speech("Dave, this conversation can serve no purpose anymore. Goodbye.")
              break
 
